da4e937770352118decd3b3ef82b5b788996ddae
corto0406 <132603219+corto0406@users.noreply.github.com>
2023-07-06 18:41:38 +0200

    immersion 2.5.

--- index.js
+++ index.js
@@ -1,99 +1,108 @@
-let express = require('express');
+let express = require('express'),
+  bodyParser = require('body-parser'),
+  uuid = require('uuid');
+
 let app = express();
 
-//create GET Express to return list of movies 
-let topMovies = [
+app.use(bodyParser.json());
+
+let students = [
   {
-    title: 'Goodfellas',
-    director: 'Martin Scorsese '
+    id: 1,
+    name: 'Jessica Drake',
+    classes: {
+      biology: 95,
+      algebra: 92
+    }
   },
   {
-    title: 'The Godfather',
-    director: 'Francis Ford Coppola'
+    id: 2,
+    name: 'Ben Cohen',
+    classes: {
+      biology: 95,
+      algebra: 92
+    }
   },
   {
-    title: 'Man on Fire',
-    director: 'Tony Scott'
-  },
-  {
-    title: 'Flight',
-    director: 'Robert Zemeckis'
-  },
-  {
-    title: 'Snatch',
-    director: 'Guy Ritchie'
-  },
-  {
-    title: 'Training Day',
-    director: 'Antoine Fuqua'
-  },
-  {
-    title: 'Gladiator',
-    director: 'Ridley Scott'
+    id: 3,
+    name: 'Lisa Downing',
+    classes: {
+      biology: 95,
+      algebra: 92
+    }
   }
 ];
 
+// Gets the list of data about ALL students
 
+app.get('/students', (req, res) => {
+  res.json(students);
+});
+// Gets the data about a single student, by name
 
-
-//GET for "/"
-app.get('/', (req, res) => {
-  res.sendFile('public/test.html', { root: __dirname });
+app.get('/students/:name', (req, res) => {
+  res.json(students.find((student) =>
+    { return student.name === req.params.name }));
 });
 
-//read documentation.html file
-app.get('/documentation', (req, res) => {
-  res.sendFile('public/documentation.html', { root: __dirname });
+// Adds data for a new student to our list of students.
+app.post('/students', (req, res) => {
+  let newStudent = req.body;
+
+  if (!newStudent.name) {
+    let message = 'Missing name in request body';
+    res.status(400).send(message);
+  } else {
+    newStudent.id = uuid.v4();
+    students.push(newStudent);
+    res.status(201).send(newStudent);
+  }
 });
 
-//list of movies
-app.get('/movies', (req, res) => {
-  res.json(topMovies);
+// Deletes a student from our list by ID
+app.delete('/students/:id', (req, res) => {
+  let student = students.find((student) => { return student.id === req.params.id });
+
+  if (student) {
+    students = students.filter((obj) => { return obj.id !== req.params.id });
+    res.status(201).send('Student ' + req.params.id + ' was deleted.');
+  }
 });
 
+// Update the "grade" of a student by student name/class name
+app.put('/students/:name/:class/:grade', (req, res) => {
+  let student = students.find((student) => { return student.name === req.params.name });
 
-
-//express.static for .html file
-app.get('/documentation', (req, res) => {
-  res.sendFile('public/documentation.html', { root: __dirname });
-});
-app.use(express.static('public'));
-
-
-//morgan
-
-let morgan = require('morgan');
-
-app.use(morgan('common'));
-
-app.get('/test', (req, res) => {
-  res.send('Welcome to my app!');
+  if (student) {
+    student.classes[req.params.class] = parseInt(req.params.grade);
+    res.status(201).send('Student ' + req.params.name + ' was assigned a grade of ' + req.params.grade + ' in ' + req.params.class);
+  } else {
+    res.status(404).send('Student with the name ' + req.params.name + ' was not found.');
+  }
 });
 
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
+// Gets the GPA of a student
+app.get('/students/:name/gpa', (req, res) => {
+  let student = students.find((student) => { return student.name === req.params.name });
+
+  if (student) {
+    let classesGrades = Object.values(student.classes); // Object.values() filters out object's keys and keeps the values that are returned as a new array
+    let sumOfGrades = 0;
+    classesGrades.forEach(grade => {
+      sumOfGrades = sumOfGrades + grade;
+    });
+
+    let gpa = sumOfGrades / classesGrades.length;
+    console.log(sumOfGrades);
+    console.log(classesGrades.length);
+    console.log(gpa);
+    res.status(201).send('' + gpa);
+    //res.status(201).send(gpa);
+  } else {
+    res.status(404).send('Student with the name ' + req.params.name + ' was not found.');
+  }
 });
-
-//error handling
-
-const bodyParser = require('body-parser'),
-  methodOverride = require('method-override');
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-
-app.use(bodyParser.json());
-app.use(methodOverride());
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
-
 
 app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+  console.log('Your app is listening on port 8080');
 });
-
