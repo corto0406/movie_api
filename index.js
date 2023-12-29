d160255c82dd3acf32e43a0cea537589b8470670
corto0406 <132603219+corto0406@users.noreply.github.com>
2023-12-29 20:31:21 +0100

    index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnified
 
 
 const cors = require('cors');
-let allowedOrigins = ['http://testsite.com','http://localhost:1234','https://movie-table.netlify.app'];
+let allowedOrigins = ['http://testsite.com','http://localhost:1234','https://movie-table.netlify.app','http://localhost:4200'];
 
 app.use(cors({
 	origin: (origin, callback) => {
