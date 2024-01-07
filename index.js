8c81afae51d2a89dc734e6d75810c96ddbf3b083
corto0406 <132603219+corto0406@users.noreply.github.com>
2024-01-07 14:15:35 +0100

    iundex.js with JSDoc comments

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/**
+ * @fileoverview Main application file for the Movie API project.
+ * @module index
+ */
+
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
@@ -8,21 +13,29 @@ const mongoose = require('mongoose');
 const Models = require('./models.js');
 const { check, validationResult } = require('express-validator');
 
-const Movies = Models.Movie;
-const Users = Models.User;
+// Importing the passport configuration
+const passport = require('passport');
+require('./passport');
 
-//mongoose.connect('mongodb://localhost:27017/cfDB', { useNewUrlParser: true, useUnifiedTopology: true });
+/**
+ * Database connection using mongoose.
+ * @name Database Connection
+ * @function
+ * @memberof module:index
+ */
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
-const cors = require('cors');
-let allowedOrigins =  ['http://testsite.com', 'http://localhost:1234', 'https://movie-table.netlify.app', 'http://localhost:4200', 'https://corto0406.github.io/test/', 'https://corto0406.github.io'];
-
-
+/**
+ * Express middleware to handle CORS.
+ * @name Cors Middleware
+ * @function
+ * @memberof module:index
+ */
+let allowedOrigins = ['http://testsite.com', 'http://localhost:1234', 'https://movie-table.netlify.app', 'http://localhost:4200', 'https://corto0406.github.io/test/', 'https://corto0406.github.io'];
 app.use(cors({
 	origin: (origin, callback) => {
 		if (!origin) return callback(null, true);
-		if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+		if (allowedOrigins.indexOf(origin) === -1) {
 			let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
 			return callback(new Error(message), false);
 		}
@@ -32,13 +45,22 @@ app.use(cors({
 
 app.use(bodyParser.json());
 
+/**
+ * Middleware for user authentication.
+ * @name Authentication Middleware
+ * @function
+ * @memberof module:index
+ */
 let auth = require('./auth')(app);
-
-const passport = require('passport');
-require('./passport');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
+/**
+ * Creating a write stream for access logs.
+ * @name Access Log Stream
+ * @type {WriteStream}
+ * @memberof module:index
+ */
 const accessLogScream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
 	flags: 'a'
 });
@@ -46,14 +68,24 @@ app.use(morgan('combined', { steam: accessLogScream }));
 
 app.use(express.static('public'));
 
+/**
+ * Express route for the home page.
+ * @name Home Route
+ * @function
+ * @memberof module:index
+ */
 app.get('/', (req, res) => {
 	res.send('Welcome to Movie-Place!');
 });
 
-
-//get all movies,new version
+/**
+ * Express route to get all movies.
+ * @name Get All Movies Route
+ * @function
+ * @memberof module:index
+ */
 app.get('/movies', (req, res) => {
-	Movies.find()
+	Models.Movie.find()
 		.then((movies) => {
 			res.status(201).json(movies);
 		})
@@ -63,11 +95,23 @@ app.get('/movies', (req, res) => {
 		});
 });
 
-//get movie by title
+/**
+ * Express route to get a movie by title.
+ * @name Get Movie by Title Route
+ * @function
+ * @memberof module:index
+ */
 app.get('/movies/:title', passport.authenticate('jwt', { session: false }),
+	/**
+	 * Handle the request to get a movie by title.
+	 * @function
+	 * @memberof module:index
+	 * @param {object} req - Express request object.
+	 * @param {object} res - Express response object.
+	 */
 	(req, res) => {
 		const { title } = req.params;
-		Movies.findOne({ Title: req.params.title })
+		Models.Movie.findOne({ Title: req.params.title })
 			.then((movie) => {
 				if (movie) {
 					res.status(200).json(movie);
@@ -81,10 +125,22 @@ app.get('/movies/:title', passport.authenticate('jwt', { session: false }),
 			});
 	});
 
-//get movies by genre name
+/**
+ * Express route to get movies by genre name.
+ * @name Get Movies by Genre Route
+ * @function
+ * @memberof module:index
+ */
 app.get('/movies/genres/:genreName', passport.authenticate('jwt', { session: false }),
+	/**
+	 * Handle the request to get movies by genre name.
+	 * @function
+	 * @memberof module:index
+	 * @param {object} req - Express request object.
+	 * @param {object} res - Express response object.
+	 */
 	(req, res) => {
-		Movies.find({ 'Genre.Name': req.params.genreName })
+		Models.Movie.find({ 'Genre.Name': req.params.genreName })
 			.then((movies) => {
 				res.status(200).json(movies);
 			})
@@ -93,10 +149,22 @@ app.get('/movies/genres/:genreName', passport.authenticate('jwt', { session: fal
 			});
 	});
 
-//find movie by director name
+/**
+ * Express route to find movies by director name.
+ * @name Find Movies by Director Route
+ * @function
+ * @memberof module:index
+ */
 app.get('/movies/directors/:directorsName', passport.authenticate('jwt', { session: false }),
+	/**
+	 * Handle the request to find movies by director name.
+	 * @function
+	 * @memberof module:index
+	 * @param {object} req - Express request object.
+	 * @param {object} res - Express response object.
+	 */
 	(req, res) => {
-		Movies.find({ 'Director.Name': req.params.directorsName })
+		Models.Movie.find({ 'Director.Name': req.params.directorsName })
 			.then((movies) => {
 				res.status(200).json(movies);
 			})
@@ -105,16 +173,20 @@ app.get('/movies/directors/:directorsName', passport.authenticate('jwt', { sessi
 			});
 	});
 
-//add a user
+/**
+ * Express route to add a user.
+ * @name Add User Route
+ * @function
+ * @memberof module:index
+ */
 app.post('/users', async (req, res) => {
-	let hashedPassword = Users.hashPassword(req.body.Password);
-	await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
+	let hashedPassword = Models.User.hashPassword(req.body.Password);
+	await Models.User.findOne({ Username: req.body.Username })
 		.then((user) => {
 			if (user) {
-				//If the user is found, send a response that it already exists
 				return res.status(400).send(req.body.Username + ' already exists');
 			} else {
-				Users
+				Models.User
 					.create({
 						Username: req.body.Username,
 						Password: hashedPassword,
@@ -134,10 +206,22 @@ app.post('/users', async (req, res) => {
 		});
 });
 
-//find user by username
+/**
+ * Express route to find user by username.
+ * @name Find User by Username Route
+ * @function
+ * @memberof module:index
+ */
 app.get('/users/:Username', passport.authenticate('jwt', { session: false }),
+	/**
+	 * Handle the request to find user by username.
+	 * @function
+	 * @memberof module:index
+	 * @param {object} req - Express request object.
+	 * @param {object} res - Express response object.
+	 */
 	(req, res) => {
-		Users.findOne({ Username: "Bobika" })
+		Models.User.findOne({ Username: "Bobika" })
 			.then((user) => {
 				res.json(user);
 			})
@@ -147,9 +231,14 @@ app.get('/users/:Username', passport.authenticate('jwt', { session: false }),
 			});
 	});
 
-//get all users
+/**
+ * Express route to get all users.
+ * @name Get All Users Route
+ * @function
+ * @memberof module:index
+ */
 app.get('/users', (req, res) => {
-	Users.find()
+	Models.User.find()
 		.then((users) => {
 			res.status(201).json(users);
 		})
@@ -159,7 +248,12 @@ app.get('/users', (req, res) => {
 		});
 });
 
-//update user info by user name
+/**
+ * Express route to update user info by username.
+ * @name Update User Info Route
+ * @function
+ * @memberof module:index
+ */
 app.put('/users/:userName',
 	[
 		check('Username', 'Username is required').isLength({ min: 5 }),
@@ -169,14 +263,21 @@ app.put('/users/:userName',
 		check('Email', 'Email does not appear to be valid').isEmail()
 	],
 	passport.authenticate('jwt', { session: false }),
+	/**
+	 * Handle the request to update user info by username.
+	 * @function
+	 * @memberof module:index
+	 * @param {object} req - Express request object.
+	 * @param {object} res - Express response object.
+	 */
 	(req, res) => {
 		let errors = validationResult(req);
 		if (!errors.isEmpty()) {
 			return res.status(422).json({ errors: errors.array() });
 		}
-		let hashedPassword = Users.hashPassword(req.body.Password);
+		let hashedPassword = Models.User.hashPassword(req.body.Password);
 
-		Users.findOneAndUpdate(
+		Models.User.findOneAndUpdate(
 			{ Username: req.params.userName },
 			{
 				$set: {
@@ -201,10 +302,22 @@ app.put('/users/:userName',
 			});
 	});
 
-//add movie to user's favorites
+/**
+ * Express route to add movie to user's favorites.
+ * @name Add Movie to Favorites Route
+ * @function
+ * @memberof module:index
+ */
 app.post('/users/:userName/movies/:MovieID', passport.authenticate('jwt', { session: false }),
+	/**
+	 * Handle the request to add movie to user's favorites.
+	 * @function
+	 * @memberof module:index
+	 * @param {object} req - Express request object.
+	 * @param {object} res - Express response object.
+	 */
 	(req, res) => {
-		Users.findOneAndUpdate(
+		Models.User.findOneAndUpdate(
 			{ Username: req.params.userName },
 			{
 				$addToSet: { FavoriteMovies: req.params.MovieID }
@@ -224,10 +337,22 @@ app.post('/users/:userName/movies/:MovieID', passport.authenticate('jwt', { sess
 			});
 	});
 
-// delete movie from favorites
+/**
+ * Express route to delete movie from user's favorites.
+ * @name Delete Movie from Favorites Route
+ * @function
+ * @memberof module:index
+ */
 app.delete('/users/:userName/movies/:MovieID', passport.authenticate('jwt', { session: false }),
+	/**
+	 * Handle the request to delete movie from user's favorites.
+	 * @function
+	 * @memberof module:index
+	 * @param {object} req - Express request object.
+	 * @param {object} res - Express response object.
+	 */
 	(req, res) => {
-		Users.findOneAndUpdate(
+		Models.User.findOneAndUpdate(
 			{ Username: req.params.userName },
 			{
 				$pull: { FavoriteMovies: req.params.MovieID }
@@ -247,10 +372,22 @@ app.delete('/users/:userName/movies/:MovieID', passport.authenticate('jwt', { se
 			});
 	});
 
-// delete user by username
+/**
+ * Express route to delete user by username.
+ * @name Delete User Route
+ * @function
+ * @memberof module:index
+ */
 app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
+	/**
+	 * Handle the request to delete user by username.
+	 * @function
+	 * @memberof module:index
+	 * @param {object} req - Express request object.
+	 * @param {object} res - Express response object.
+	 */
 	(req, res) => {
-		Users.findOneAndRemove({ Username: req.params.Username })
+		Models.User.findOneAndRemove({ Username: req.params.Username })
 			.then((user) => {
 				if (!user) {
 					res.status(400).send(req.params.Username + ' was not found');
@@ -264,15 +401,24 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false }),
 			});
 	});
 
-
+/**
+ * Express error handling middleware.
+ * @name Error Handling Middleware
+ * @function
+ * @memberof module:index
+ */
 app.use((err, req, res, next) => {
 	console.error(err.stack);
 	res.status(500).send("Something Broke");
 });
 
-
-
+/**
+ * Start the server and listen on a specified port.
+ * @name Server Start
+ * @function
+ * @memberof module:index
+ */
 const port = process.env.PORT || 3000;
-app.listen(port, '0.0.0.0',() => {
- console.log('Listening on Port ' + port);
+app.listen(port, '0.0.0.0', () => {
+	console.log('Listening on Port ' + port);
 });
